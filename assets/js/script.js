b8284682cbc7002f22950bf3cd4b7e4b4233962d
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-08-06 11:58:30 -0400

    shows high scores, high score button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,13 @@ var resultsPElement = document.querySelector("#results");
 
 //Buttons
 var letsStartBtnElement = document.querySelector("#letsStartBtn");
+var tryAgainBtnElement = document.querySelector("#tryAgainBtn");
+var submitBtnElement = document.querySelector("#submitBtn");
 
 //Screens
 var startQuizScreenElement = document.querySelector("#startQuizScreen");
 var quizQuestionsElement = document.querySelector("#quizQuestionsScreen");
+var showHSEl = document.querySelector("#showHS");
 
 //Timer
 var counter = 60;
@@ -108,12 +111,86 @@ var resultsScreenEl = document.querySelector("#enterHS");
 function endQuiz() {
   console.log("quiz ended");
   clearInterval(startCountdown);
-  //  get final score
-  //display/save in local storage
-  //results screen
+  //enter name
   resultsScreenEl.classList.remove("hidden");
   resultsScreenEl.style.display = "block";
 }
+
+//capture score
+submitBtnElement.addEventListener("click", function () {
+  console.log("Saving score?");
+  var hsName = document.querySelector("#hsName").value;
+  console.log("hs name", hsName);
+  var hsScore = counter;
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  var newScore = {
+    score: hsScore,
+    initials: hsName,
+  };
+  console.log(newScore);
+  highScores.push(newScore);
+
+  saveScore(highScores);
+  //showing score
+  resultsScreenEl.classList.add("hidden");
+  resultsScreenEl.style.display = "none";
+  quizQuestionsElement.classList.add("hidden");
+  showHSEl.classList.remove("hidden");
+  showHSEl.style.display = "block";
+  showScores();
+});
+
+//save final score
+var saveScore = function (highScores) {
+  localStorage.setItem("score", JSON.stringify(highScores));
+};
+
+//Show final scores
+var showScores = function () {
+  const scores = JSON.parse(localStorage.getItem("score"));
+  console.log(JSON.stringify(scores));
+  //button to go back to home page
+  quizQuestionsElement.addEventListener("click", function () {
+    console.log("headed back to home page");
+  });
+  for (var i = 0; i < scores.length; i++) {
+    var initial = i.initials; //pulls just the initial out
+    var score = i.score; //pulls just the score out
+    var li = document.createElement("li"); //creates the list item
+    var list = document.getElementById("hsList"); // ordered or unordered list where you want the list to show
+    li.appendChild(
+      document.createTextNode(scores[i].initials + ": " + scores[i].score)
+    ); //adds current initial to li
+    list.appendChild(li);
+    console.log(li);
+  }
+};
+
+// score-> homepage function
+tryAgainBtnElement.addEventListener("click", function () {
+  console.log("home page");
+  showHSEl.classList.add("hidden");
+  showHSEl.style.display = "none";
+  startQuizScreenElement.classList.remove("hidden");
+  counter = 60;
+  currentQuestionIndex = 0;
+  document.getElementById("countDown").textContent = counter;
+});
+
+//view high scores
+var VHS = document.querySelector("#VHS");
+VHS.addEventListener("click", function () {
+  counter = 60;
+  document.getElementById("countDown").textContent = counter;
+  console.log("view high score screen");
+  showScores();
+  startQuizScreenElement.classList.add("hidden");
+  resultsScreenEl.classList.add("hidden");
+  resultsScreenEl.style.display = "none";
+  quizQuestionsElement.classList.add("hidden");
+  showHSEl.classList.remove("hidden");
+  showHSEl.style.display = "block";
+});
 
 letsStartBtnElement.addEventListener("click", function () {
   console.log("start quiz");
